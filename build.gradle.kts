4bfe3781adf5001dcef8c5516e3a19909a3496a1
gpremo <gpremo@rationalenterprise.com>
2023-12-14 11:57:35 -0500

    change build.gradle to test env

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     java
     id("org.springframework.boot") version "3.1.4"
     id("io.spring.dependency-management") version "1.1.3"
-    id("com.dorongold.task-tree") version "2.1.1"
+//    id("com.dorongold.task-tree") version "2.1.1"
 }
 
 group = "com.garretpremo"
